b1e857cece479b34cd5d2d0c00ec9db534d1e84b
arwasowski <arwasowski@gmail.com>
2019-07-28 20:49:26 -0400

    Updates

---
+++ precache-manifest.e2c0d2c03bac71887d0a915f147f3929.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3054560d3e841a2c5343d19a9cc87fb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "232daf8e7966641b16f3",
+    "url": "/monsters-rolodex/static/css/main.54cdbce1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "232daf8e7966641b16f3",
+    "url": "/monsters-rolodex/static/js/main.6eb390ac.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
